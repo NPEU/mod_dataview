06cbf5b3bce6e87822c5677f5ad82e0ddf2f9a83
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-06-05 16:41:48 +0100

    WiP

--- tmpl/default.php
+++ tmpl/default.php
@@ -12,11 +12,31 @@ defined('_JEXEC') or die;
 
 $doc = JFactory::getDocument();
 
-?>
+$data_src        = $params->get('data_src');
+$data_tpl        = $params->get('data_tpl');
+$data_src_err    = $params->get('data_src_err');
+$data_decode_err = $params->get('data_decode_err');
 
-<?php if ($params->get('show_title')): ?>
-<p><?php echo $params->get('title'); ?></p>
-<?php endif; ?>
-<?php if ($params->get('thing')): ?>
-<p><?php echo $params->get('thing'); ?></p>
-<?php endif; ?>
+// Allow for relative data src URLs:
+if (strpos($data_src, 'http') !== 0) {
+    $s        = empty($_SERVER['SERVER_PORT']) ? '' : ($_SERVER['SERVER_PORT'] == '443') ? 's' : '';
+    $protocol = preg_replace('#/.*#',  $s, strtolower($_SERVER['SERVER_PROTOCOL']));
+    $domain   = $protocol.'://'.$_SERVER['SERVER_NAME'];
+    $data_src = $domain . '/' . trim($data_src, '/');
+}
+
+if (!$data = file_get_contents($data_src)) {
+    $output = Markdown::defaultTransform($data_src_err);
+} else {
+    if (!$json = json_decode($data)) {
+        $output = Markdown::defaultTransform($data_decode_err);
+    } else {
+        $twig = ModBrochureHelper::getTwig(array(
+            'tpl' => $data_tpl
+        ));
+
+        $output = $twig->render('tpl', array('data' => $json));
+    }
+}
+?>
+<?php echo $output; ?>
