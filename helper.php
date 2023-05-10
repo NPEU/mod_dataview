eba269a7b9b2842a2638c6bf7ad6b6bc064cd80a
Andy Kirk <andy@andykirk.net>
2023-05-10 16:40:18 +0100

    Added Highcharts more script.

--- helper.php
+++ helper.php
@@ -38,6 +38,7 @@ class ModDataviewHelper
 
         if ($params->get('highcharts', false)) {
             $doc->addScript('https://code.highcharts.com/highcharts.js');
+            $doc->addScript('https://code.highcharts.com/highcharts-more.js');
             $doc->addScript('https://code.highcharts.com/modules/exporting.js');
             $doc->addScript('https://code.highcharts.com/modules/export-data.js');
             $doc->addScript('https://code.highcharts.com/modules/accessibility.js');
